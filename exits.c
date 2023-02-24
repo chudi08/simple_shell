023da325c2d4a92edf3ddc7af376b67784e5d849
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2023-02-24 23:58:20 +0100

    exits.c

--- exits.c
+++ exits.c
@@ -48,3 +48,27 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	while (src[j] != '\0' && j < n)
 	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
